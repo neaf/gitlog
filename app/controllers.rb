528805120712067068178f14acf1039330a4b7da
Tomasz Werbicki <tomasz@werbicki.net>
2010-07-15 13:44:36 +0200

    Add repository list.

--- app/controllers.rb
+++ app/controllers.rb
@@ -1,6 +1,11 @@
 Gitlog.controllers  do
   get :index, map: "/" do
-    @log_items = LogItem.all.order_by([:created_at, :desc])
+    @log_items = LogItem.order_by(:created_at.desc)
+    render "log_items/index"
+  end
+
+  get :repository, map: "/:repository" do
+    @log_items = LogItem.where(:repository => params[:repository]).order_by(:created_by.desc)
     render "log_items/index"
   end
 
